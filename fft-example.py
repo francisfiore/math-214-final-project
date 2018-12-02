69d074f8f23fa1ec998cdacb19111222e377cd71
Francis Fiore <francisfiore98@gmail.com>
2018-12-02 10:45:40 -0500

    fft2 works

--- fft-example.py
+++ fft-example.py
@@ -1,9 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from imageio import imread
+import math
 import sys
 import os
 
 
+im = imread("neri.jpeg", as_gray=True)
+trans = np.fft.fft2(im)
+trans = np.log10(abs(trans)); 
+plt.imshow(trans);
+plt.show()
+
+sys.exit()
 
 t = np.arange(256)
 sp = np.fft.fft(np.sin(t))
