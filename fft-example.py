b08ae9f0deaeacea61cf4c57e3b2c2b69b005aa5
Francis Fiore <francisfiore98@gmail.com>
2018-12-02 10:13:15 -0500

    venv conversion

--- fft-example.py
+++ fft-example.py
@@ -1,3 +1,12 @@
-from numpy import fft
+import numpy as np
+import matplotlib.pyplot as plt
 import sys
 import os
+
+
+
+t = np.arange(256)
+sp = np.fft.fft(np.sin(t))
+freq = np.fft.fftfreq(t.shape[-1])
+plt.plot(freq, sp.real, freq, sp.imag)
+plt.show()
